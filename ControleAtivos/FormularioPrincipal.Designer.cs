d362695611f826df550189dc744939cf2aa5393f
icaro-freitas <icaro_freitas2014@outlook.com>
2023-03-20 20:00:56 -0300

    Foram adicionadas novas paginas de interface.

--- ControleAtivos/FormularioPrincipal.Designer.cs
+++ ControleAtivos/FormularioPrincipal.Designer.cs
@@ -32,9 +32,10 @@ namespace ControleAtivos
             this.components = new System.ComponentModel.Container();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.Consulta = new System.Windows.Forms.TabPage();
+            this.tabConsulta = new System.Windows.Forms.TabPage();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabComunicacaoSerial = new System.Windows.Forms.TabPage();
+            this.btnProcurar = new System.Windows.Forms.Button();
             this.lblStatusComunicacao = new System.Windows.Forms.Label();
             this.btnDesconectar = new System.Windows.Forms.Button();
             this.btnConectar = new System.Windows.Forms.Button();
@@ -44,33 +45,36 @@ namespace ControleAtivos
             this.lblPortaCom = new System.Windows.Forms.Label();
             this.lblConfiguracoes = new System.Windows.Forms.Label();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.btnProcurar = new System.Windows.Forms.Button();
+            this.tabGerenciaAtivos = new System.Windows.Forms.TabPage();
+            this.tabGerenciarSalas = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.tabControl1.SuspendLayout();
-            this.Consulta.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.tabConsulta.SuspendLayout();
+            this.tabComunicacaoSerial.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.Consulta);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabConsulta);
+            this.tabControl1.Controls.Add(this.tabGerenciaAtivos);
+            this.tabControl1.Controls.Add(this.tabGerenciarSalas);
+            this.tabControl1.Controls.Add(this.tabComunicacaoSerial);
             this.tabControl1.Location = new System.Drawing.Point(-2, -2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1053, 692);
             this.tabControl1.TabIndex = 0;
             // 
-            // Consulta
+            // tabConsulta
             // 
-            this.Consulta.Controls.Add(this.comboBox1);
-            this.Consulta.Location = new System.Drawing.Point(4, 22);
-            this.Consulta.Name = "Consulta";
-            this.Consulta.Padding = new System.Windows.Forms.Padding(3);
-            this.Consulta.Size = new System.Drawing.Size(1045, 666);
-            this.Consulta.TabIndex = 0;
-            this.Consulta.Text = "Consulta Ativos";
-            this.Consulta.UseVisualStyleBackColor = true;
+            this.tabConsulta.Controls.Add(this.comboBox1);
+            this.tabConsulta.Location = new System.Drawing.Point(4, 22);
+            this.tabConsulta.Name = "tabConsulta";
+            this.tabConsulta.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConsulta.Size = new System.Drawing.Size(1045, 666);
+            this.tabConsulta.TabIndex = 0;
+            this.tabConsulta.Text = "Consulta Ativos";
+            this.tabConsulta.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
@@ -81,24 +85,35 @@ namespace ControleAtivos
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 0;
             // 
-            // tabPage2
+            // tabComunicacaoSerial
             // 
-            this.tabPage2.Controls.Add(this.btnProcurar);
-            this.tabPage2.Controls.Add(this.lblStatusComunicacao);
-            this.tabPage2.Controls.Add(this.btnDesconectar);
-            this.tabPage2.Controls.Add(this.btnConectar);
-            this.tabPage2.Controls.Add(this.cboBaudRate);
-            this.tabPage2.Controls.Add(this.cboPortaCOM);
-            this.tabPage2.Controls.Add(this.lblBaudRate);
-            this.tabPage2.Controls.Add(this.lblPortaCom);
-            this.tabPage2.Controls.Add(this.lblConfiguracoes);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1045, 666);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Comunicação Serial";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabComunicacaoSerial.Controls.Add(this.btnProcurar);
+            this.tabComunicacaoSerial.Controls.Add(this.lblStatusComunicacao);
+            this.tabComunicacaoSerial.Controls.Add(this.btnDesconectar);
+            this.tabComunicacaoSerial.Controls.Add(this.btnConectar);
+            this.tabComunicacaoSerial.Controls.Add(this.cboBaudRate);
+            this.tabComunicacaoSerial.Controls.Add(this.cboPortaCOM);
+            this.tabComunicacaoSerial.Controls.Add(this.lblBaudRate);
+            this.tabComunicacaoSerial.Controls.Add(this.lblPortaCom);
+            this.tabComunicacaoSerial.Controls.Add(this.lblConfiguracoes);
+            this.tabComunicacaoSerial.Location = new System.Drawing.Point(4, 22);
+            this.tabComunicacaoSerial.Name = "tabComunicacaoSerial";
+            this.tabComunicacaoSerial.Padding = new System.Windows.Forms.Padding(3);
+            this.tabComunicacaoSerial.Size = new System.Drawing.Size(1045, 666);
+            this.tabComunicacaoSerial.TabIndex = 1;
+            this.tabComunicacaoSerial.Text = "Comunicação Serial";
+            this.tabComunicacaoSerial.UseVisualStyleBackColor = true;
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcurar.Location = new System.Drawing.Point(333, 71);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcurar.TabIndex = 8;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.BtnProcurar_Click);
             // 
             // lblStatusComunicacao
             // 
@@ -191,16 +206,25 @@ namespace ControleAtivos
             this.lblConfiguracoes.TabIndex = 0;
             this.lblConfiguracoes.Text = "Configurações";
             // 
-            // btnProcurar
+            // tabGerenciaAtivos
             // 
-            this.btnProcurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProcurar.Location = new System.Drawing.Point(333, 71);
-            this.btnProcurar.Name = "btnProcurar";
-            this.btnProcurar.Size = new System.Drawing.Size(75, 23);
-            this.btnProcurar.TabIndex = 8;
-            this.btnProcurar.Text = "Procurar";
-            this.btnProcurar.UseVisualStyleBackColor = true;
-            this.btnProcurar.Click += new System.EventHandler(this.BtnProcurar_Click);
+            this.tabGerenciaAtivos.Location = new System.Drawing.Point(4, 22);
+            this.tabGerenciaAtivos.Name = "tabGerenciaAtivos";
+            this.tabGerenciaAtivos.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGerenciaAtivos.Size = new System.Drawing.Size(1045, 666);
+            this.tabGerenciaAtivos.TabIndex = 2;
+            this.tabGerenciaAtivos.Text = "Gerenciar Ativos";
+            this.tabGerenciaAtivos.UseVisualStyleBackColor = true;
+            // 
+            // tabGerenciarSalas
+            // 
+            this.tabGerenciarSalas.Location = new System.Drawing.Point(4, 22);
+            this.tabGerenciarSalas.Name = "tabGerenciarSalas";
+            this.tabGerenciarSalas.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGerenciarSalas.Size = new System.Drawing.Size(1045, 666);
+            this.tabGerenciarSalas.TabIndex = 3;
+            this.tabGerenciarSalas.Text = "Gerenciar Salas";
+            this.tabGerenciarSalas.UseVisualStyleBackColor = true;
             // 
             // FormularioPrincipal
             // 
@@ -214,9 +238,9 @@ namespace ControleAtivos
             this.Load += new System.EventHandler(this.FormularioPrincipal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.tabControl1.ResumeLayout(false);
-            this.Consulta.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
+            this.tabConsulta.ResumeLayout(false);
+            this.tabComunicacaoSerial.ResumeLayout(false);
+            this.tabComunicacaoSerial.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -225,8 +249,8 @@ namespace ControleAtivos
 
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage Consulta;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabConsulta;
+        private System.Windows.Forms.TabPage tabComunicacaoSerial;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label lblConfiguracoes;
         private System.Windows.Forms.Label lblPortaCom;
@@ -238,6 +262,8 @@ namespace ControleAtivos
         private System.Windows.Forms.Label lblStatusComunicacao;
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.TabPage tabGerenciaAtivos;
+        private System.Windows.Forms.TabPage tabGerenciarSalas;
     }
 }
 
